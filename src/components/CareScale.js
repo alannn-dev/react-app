d433b97014f553cd0293920e7378fbd1fae7ed4a
Alan <boulay.alan@gmail.com>
2023-02-26 16:52:42 +0100

    Add sun+water image in CareScale component

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,19 +1,30 @@
+// Care scale 
+
 /* function CareScale(props) {
   const scaleValue = props.scaleValue */
 
+  import Sun from '../assets/sun.svg'
+  import Water from '../assets/water.svg'
+  
   // const {scaleValue, careType} = props
   function CareScale({ scaleValue, careType }) {
-    const range = [1, 2, 3]
-    
-    const scaleType = careType === 'light' ? '☀️' : '💧'
+      const range = [1, 2, 3]
+      const scaleType =
+          careType === 'light' ? (
+              <img src={Sun} alt='sun-icon' />
+          ) : (
+              <img src={Water} alt='water-icon' />
+          )
 
-    return (
-        <div>
-            {/* Le "KEY" attribué à chaque icône est l'élément actuel converti en chaîne de caractères.*/}
-            {range.map((rangeElem) => scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
-            )}
-        </div>
-    )
-}
-    
-export default CareScale
+      return (
+          <div>
+              {range.map((rangeElem) =>
+                  scaleValue >= rangeElem ? (
+                      <span key={rangeElem.toString()}>{scaleType}</span>
+                  ) : null
+              )}
+          </div>
+      )
+  }
+  
+  export default CareScale
