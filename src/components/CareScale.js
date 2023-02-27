1e4594fb012e34de253dc94e96ccee1addf3b99f
Alan <boulay.alan@gmail.com>
2023-02-27 16:33:47 +0100

    Add new event onClick

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -2,11 +2,17 @@
 
 /* function CareScale(props) {
   const scaleValue = props.scaleValue */
-
   import Sun from '../assets/sun.svg'
   import Water from '../assets/water.svg'
   
-  // const {scaleValue, careType} = props
+  // Ici, il s'agit d'une manière de faire.
+  //Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
+  const quantityLabel = {
+      1: 'peu',
+      2: 'modérément',
+      3: 'beaucoup'
+  }
+  
   function CareScale({ scaleValue, careType }) {
       const range = [1, 2, 3]
       const scaleType =
@@ -15,14 +21,22 @@
           ) : (
               <img src={Water} alt='water-icon' />
           )
-
+  
       return (
-          <div>
+          <div
+              onClick={() =>
+                  alert(
+                      `Cette plante requiert ${quantityLabel[scaleValue]} ${
+                          careType === 'light' ? 'de lumière' : "d'arrosage"
+                      }`
+                  )
+              }
+          >
               {range.map((rangeElem) =>
                   scaleValue >= rangeElem ? (
                       <span key={rangeElem.toString()}>{scaleType}</span>
                   ) : null
-              )}
+                  )}
           </div>
       )
   }
