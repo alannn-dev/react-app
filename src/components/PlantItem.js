24e058a0724041abb18dd766a4d1ad1e4dca8878
Alan <boulay.alan@gmail.com>
2023-02-28 20:12:16 +0100

    Add price on each product card

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,11 +1,11 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function PlantItem({ id, cover, name, water, light, price }) {
 	return (
 		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
-			{name}
+			<span className="name-and-price">{name} {price}€</span>
 			<div>
 				<CareScale careType='water' scaleValue={water} />
 				<CareScale careType='light' scaleValue={light} />
